2d886e1ed7d7bc79ea2192fef80ce06601d946ab
Disha <dishaacharyaa04@gmail.com>
2025-04-17 14:36:56 +0530

    Initial

---
+++ javascript/newFile.js
@@ -0,0 +1,26 @@
+/*var age=10;
+var name="ABC"
+var name="XYZ"
+var age=14;
+age=98;
+name="pqr"
+
+let name="nknf";
+let age=098;
+var age=10;
+let gender="male";
+const institute="xworkz"
+document.writeln(age)*/
+
+let emp1={id:12, employeeName:"aa",salary:1000}
+let emp2= new Object();
+emp2.id=13;
+emp2.employeeName="bb";
+emp2.salary=50000;
+emp2.id=14;
+
+function emp3(id, employeeName,salary){
+    this.id=id;
+    this.employeeName=employeeName;
+    this.salary=salary;
+}
